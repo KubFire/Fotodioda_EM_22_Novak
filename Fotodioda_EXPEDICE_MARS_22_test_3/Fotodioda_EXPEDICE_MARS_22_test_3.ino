cbc660ed80937414f6eeb4a4f1b666682b970755
KubFire <kubfire@gmail.com>
2022-08-10 19:36:31 +0200

    Fotodioda 1.02
    Díky BOHU, je to done.
    Rework systému hodin, protože si dělal co chtěl až jsem z toho brečel.
    Rework systému určování poledne, ted je zpetný
    Přidání systému předpovídání hodin a tak aby fungoval.
    fuuf. ted jeste dodelat centralni kecani ci jak to navzat

--- Fotodioda_EXPEDICE_MARS_22_test_3/Fotodioda_EXPEDICE_MARS_22_test_3.ino
+++ Fotodioda_EXPEDICE_MARS_22_test_3/Fotodioda_EXPEDICE_MARS_22_test_3.ino
@@ -12,12 +12,18 @@ int IntensityNoon = 0; //globalni prommenne
 int IntensityNow = 0;
 int IntensityBefore = 0;
 int Intensity = 0;
+int IntensityMax = 0;
 int cas = 0;
 int casPoledne = 0;
 int soucasnycas = 0;
 int den = 0; //mysleno den jako svetlo nebo noc ne jako pondeli utery
 int denBefore = 0; //toto uz vypisuje
 int radek = 0;
+int timestampNoon = 0;
+int minutaNoon = 0;
+int hodinaNoon = 0;
+int hodnota = 0;
+int casMax = 0;
 
 String message;
 
@@ -25,7 +31,7 @@ const int C_VERB_DEBUG = 2;
 const int C_VERB_NORM = 1;
 const int C_VERB_LOW = 0;
 
-const int C_VERBOSITY = C_VERB_NORM;
+const int C_VERBOSITY = C_VERB_DEBUG;
 
 //----------------------------------------------------------------------------------
 
@@ -64,7 +70,11 @@ void setup() {
       String list  = myFile.readStringUntil('\n');      //ted to asi funguje
 
 //-------------INTENSITA-----------------------      
-      Intensity = list.toInt();  //list je string ale
+      Intensity = list.toInt();  //list je string ale  
+      
+      message = "Intensita světla - ";
+      message = message + Intensity;
+      Tisk(message, C_VERB_NORM);    
       
       
       IntensityBefore = IntensityNow; //Hodnota intenzity svetla ted(predchozi mereni) se ulozi do Before
@@ -72,7 +82,7 @@ void setup() {
 //----------------TIMESTAMP-A-RADEK--------------------------------      
       cas = cas +5; //hodiny se posunou o 5 minut tzv jedno mereni   
       radek++;
-
+      //Serial.println(casPoledne);
       message = "Radek ";
       message = message + radek;
       Tisk(message, C_VERB_DEBUG);      
@@ -87,9 +97,10 @@ void setup() {
       if (den == 1){
         NoonDetection();
         ZobrazCas();
-        if(casPoledne !=  0){
+        TimePredict();
+
           NightPredict(); 
-        }
+        
 
       }
 
@@ -131,7 +142,7 @@ void DayOrNight(){
   if (IntensityNow > 100){
     den = 1;
     message = "DEN";
-    Tisk(message, C_VERB_NORM);    
+    Tisk(message, C_VERB_NORM);  
   }
   
   else {
@@ -147,47 +158,59 @@ void DayOrNight(){
   
 }
 
-//-------------------------------NOON-DETECTION------------------------------- F
-void NoonDetection(){                                        
-  //Serial.print("Je poledne? - ");   
-  if(IntensityNow > IntensityBefore){  
-    if(IntensityNow > IntensityNoon){
-      IntensityNoon = IntensityNow;
-      casPoledne = cas;
-      message = "Je Poledne";
-      Tisk(message, C_VERB_DEBUG);   
-    }
-   else{
-      message = "Není Poledne";
-      Tisk(message, C_VERB_DEBUG);   
-   }       
-    
+//-------------------------------NOON-DETECTION------------------------------- FFF
+void NoonDetection(){     
+  if(IntensityMax > IntensityNow){
+    hodnota++;                             
   }
-    
+  else{
+    hodnota = 0;
+  }
+  if (hodnota == 5){ 
+        IntensityNoon = IntensityMax;
+        casPoledne = casMax;
+        Serial.println("Hodnota = 5 ");         
+        Serial.print("timestampNoon - ");
+        Serial.println(timestampNoon);
+        message = "Pred 25 min bylo Poledne";                                                                   //TOHLE UŽ FUNGUJE!!!
+        Tisk(message, C_VERB_DEBUG);
+        timestampNoon = casPoledne;  
+  }     
+  if(IntensityNow > IntensityBefore){ 
+    if(IntensityNow > IntensityMax){
+      IntensityMax = IntensityNow;
+      casMax = cas;  
+      message = "Nejvyšší naměřená hodnota so far";
+      Tisk(message, C_VERB_DEBUG);            
+   }    
+    else{
+      message = "Není Poledne";
+      Tisk(message, C_VERB_DEBUG); 
+   }          
+ }  
   else{
       message = "Není Poledne";
       Tisk(message, C_VERB_NORM); 
-  }       
- 
-  
+       
+  }        
 }
 
 
-//-------------------------JESTE NEFUNGUJE-----------------------------------
+//----------------------------ZOBRAZ-CAS--------------------------------F
 void ZobrazCas(){ //zobrazuje cas, funguje pouze po poledni
-  int hodina = 0;
+  int hodina = 0;                                                                     //Jak to udelat aby jelo jen po poledni?
   int minuta = 0;
 
 //----------------------------------------
-  if (casPoledne != 0) {
+
       soucasnycas = cas - casPoledne; //pocita kolik je prave hodin od poledne
       hodina = soucasnycas / 60 + 12; //vypocet hodin
       minuta = soucasnycas % 60; //vypocet minut
 
-      message = "AKtualni cas";
+      message = "Aktualni cas";
       message = message + hodina + ":" + minuta;
       Tisk(message, C_VERB_NORM);           
-  }
+
 }
 
 //------------------------------NIGHT-PREDICT------------------------------ F
@@ -208,3 +231,11 @@ void NightPredict(){ //predpovida zapad slunce
 }
 
 //----------------------------------------------------------------------------------------
+void TimePredict(){
+   hodinaNoon =(720 - (timestampNoon - cas))/60; 
+   minutaNoon =((720 - (timestampNoon - cas))%60);
+  
+  message = "Predpovezeny Cas";
+  message = message + hodinaNoon + ":" + minutaNoon;
+  Tisk(message, C_VERB_NORM);          
+}  
